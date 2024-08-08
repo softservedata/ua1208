ccb48564c8cd2979c0e39b1c6b3841cbf9bc33a6
Yaroslav <garasym@gmail.com>
2024-08-08 20:05:44 +0300

    Generic. List

---
+++ src/com/softserve/edu03gen/Box.java
@@ -0,0 +1,13 @@
+package com.softserve.edu03gen;
+
+public class Box {
+    private Object obj;
+
+    public Object get() {
+        return obj;
+    }
+
+    public void set(Object obj) {
+        this.obj = obj;
+    }
+}
