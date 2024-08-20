75d8a891cd0bd1da17c321d64c48e662f5c8ccc2
Yaroslav <garasym@gmail.com>
2024-08-20 18:50:30 +0300

    maven branch created

--- src/com/softserve/edu05inher/IA.java
+++
@@ -1,6 +0,0 @@
-package com.softserve.edu05inher;
-
-public interface IA {
-
-    int getI(); // public abstract
-}
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void checkAdd() {
+        double expected;
+        double actual;
+        //
+        expected = 5.0001;
+        actual = 2 + 3;
+        Assertions.assertEquals(expected, actual, 0.001);
+    }
+}
