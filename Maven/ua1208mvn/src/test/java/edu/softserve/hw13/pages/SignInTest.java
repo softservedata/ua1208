4231b435e730ce2570c503b5279ffdaf04f68d54
SvitlanaBabiuk <svetlana.babyuk1@gmail.com>
2024-10-01 23:37:28 +0300

    HW14

--- Maven/ua1208mvn/src/test/java/edu/softserve/hw13/pages/SignInTest.java
+++ Maven/ua1208mvn/src/test/java/edu/softserve/hw13/pages/SignInTest.java
@@ -3,12 +3,16 @@ package edu.softserve.hw13.pages;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SignInTest extends TestRunner {
     private SignInPage signInPage;
+    static final Logger logger = LoggerFactory.getLogger(SignInTest.class);
+
 
     @BeforeEach
     public void setUpPage() {
@@ -18,20 +22,30 @@ public class SignInTest extends TestRunner {
     @ParameterizedTest
     @MethodSource("edu.softserve.hw13.pages.SignInDataRepository#provideValidSignInData")
     public void signInValidUser(SignInDataRepository.SignInData data) {
+        logger.info("Starting test for valid user sign-in.");
 
         signInPage.clickSignInButton();
+        logger.info("Sign In button clicked.");
         signInPage.enterCredentials(data.getEmail(), data.getPassword());
+        logger.info("Credentials entered: email={} password=******", data.getEmail());
         assertEquals("З поверненням!", signInPage.getWelcomeMessage());
+        logger.info("Test for valid user sign-in passed.");
+
     }
 
     @ParameterizedTest
     @MethodSource("edu.softserve.hw13.pages.SignInDataRepository#provideInvalidSignInData")
     public void signInInvalidUser(SignInDataRepository.SignInData data) {
+        logger.info("Starting test for invalid user sign-in with email: {}", data.getEmail());
         signInPage.clickSignInButton();
+        logger.info("Sign In button clicked.");
         signInPage.enterCredentials(data.getEmail(), data.getPassword());
+        logger.info("Credentials entered: email={} password=******", data.getEmail());
         signInPage.submitSignIn();
+        logger.info("Sign In form submitted.");
 
         assertEquals(data.getEmailErrorMessage(), signInPage.getEmailErrorMessage());
+        logger.info("Test for invalid user sign-in passed.");
 
     }
 }
--- Maven/ua1208mvn/src/test/java/edu/softserve/hw13/pages/TestRunner.java
+++ Maven/ua1208mvn/src/test/java/edu/softserve/hw13/pages/TestRunner.java
@@ -6,21 +6,28 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static edu.softserve.hw13.pages.SignInTest.logger;
+
 
 public class TestRunner {
     protected static WebDriver driver;
 
     @BeforeAll
     public static void setUp() {
+        logger.info("Setting up WebDriver.");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.greencity.cx.ua/#/ubs");
+        logger.info("WebDriver setup complete, browser launched.");
     }
+
     @AfterAll
     public static void tearDown() {
         if (driver != null) {
+            logger.info("Closing WebDriver.");
             driver.quit();
+            logger.info("WebDriver closed.");
         }
     }
 
